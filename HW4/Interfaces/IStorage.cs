9343eba33158efae4d143180305f5a916649955a
partrayan <test@gmail.com>
2023-05-21 23:46:45 +0330

    Created IStorage interface

---
+++ HW4/Interfaces/IStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4.Interfaces
+{
+    public interface IStorage
+    {
+        List<User> GetAllUsers();
+
+        void AddUser(User user);
+
+    }
+}

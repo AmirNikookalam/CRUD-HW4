7baa239614e82295cc156cb23b00d379c607b15e
partrayan <test@gmail.com>
2023-05-21 23:48:40 +0330

    Created User class

---
+++ HW4/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4.Entities
+{
+    public class User
+    {
+        public int userID {get; set;}
+        public string userName { get; set;}
+        public string userMobileNumber { get; set;}
+        public DateTime userBirthday { get; set;}
+    }
+}

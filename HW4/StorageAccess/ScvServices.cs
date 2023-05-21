bc02722fa256ad9952d1d6c4a4bf497ee7a7910a
partrayan <test@gmail.com>
2023-05-21 23:47:54 +0330

    Created ScvAccess Services

---
+++ HW4/StorageAccess/ScvServices.cs
@@ -0,0 +1,22 @@
+﻿using HW4.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4.Storage
+{
+    public class scvServices : IStorage
+    {
+        public void AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

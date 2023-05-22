2bc82cc8870f06e5d5c3b548ff0d2fae7b7c6662
partrayan <test@gmail.com>
2023-05-22 11:21:05 +0330

    Created the menu that shows the user what can he/she do with this programm

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -1,12 +1,74 @@
 ﻿
 
+using HW4.Entities;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+
 namespace Program
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            User user1 = new User();
+            DateTime userBirthday = new DateTime(2023 / 12 / 30);
+            int userId = 0;
+            string userName = string.Empty;
+            string inputOption = string.Empty;
+            bool flag = true;
+
+            do
+            {
+                Console.WriteLine("--What do you want to do?" +
+                    "\n 1.Create an account " +
+                    "\n 2.Delete an account " +
+                    "\n 3.Update an account " +
+                    "\n 4.See list of users " +
+                    "\n 5.Exit" +
+                    "\n => ");
+                inputOption = Console.ReadLine();
+
+
+                switch (inputOption)
+                {
+                    //1.Create an account
+                    case "1":
+                        Console.Clear();
+
+                        break;
+
+                    //2.Delete an account
+                    case "2":
+                        Console.Clear();
+
+                        break;
+                    //3.Update an account
+                    case "3":
+                        Console.Clear();
+                        break;
+
+                    //4.See list of users
+                    case "4":
+                        Console.Clear();
+
+                        break;
+                    //5.Exit   
+                    case "5":
+
+                        flag = false;
+                        break;
+
+                    default:
+                        Console.Clear();
+
+                        Console.WriteLine("Error: Please give a number between 1 and 4!");
+                        flag = false;
+                        break;
+                }
+            } while (flag);
+
+            Console.WriteLine("G00dBye;");
         }
     }
 }

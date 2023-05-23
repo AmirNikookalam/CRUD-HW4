b62a8a187a4af58a5652a15f4dbb4d9686044ea8
partrayan <test@gmail.com>
2023-05-24 03:00:53 +0330

    Finished the GetAllUsers and AddUser method in ScvServices class

--- HW4/StorageAccess/ScvServices.cs
+++ HW4/StorageAccess/ScvServices.cs
@@ -5,19 +5,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Formats.Asn1;
+using System.Globalization;
+using CsvHelper;
 
 namespace HW4.Storage
 {
     public class ScvServices : IStorage
     {
+        static string? solutionFolderPath = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)?.Parent?.Parent?.Parent?.FullName;
+        static string dataFolderPath = Path.Combine(solutionFolderPath, "Data");
+        static string storagePath = Path.Combine(dataFolderPath, "FileDataStorage.csv");
+
         public void AddUser(User user)
         {
-            throw new NotImplementedException();
+            List<User> persons = GetAllUsers();
+            persons.Add(user);
+
+            using (var writer = new StreamWriter(storagePath))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(persons);
+            }
         }
 
         public List<User> GetAllUsers()
         {
-            throw new NotImplementedException();
+            using var streamReader = File.OpenText(storagePath);
+            using var csvReader = new CsvReader(streamReader, CultureInfo.CurrentCulture);
+
+            var users = csvReader.GetRecords<User>();
+
+            return users.ToList<User>();
         }
 
         public int Count()
